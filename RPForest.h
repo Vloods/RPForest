d0e14357b14841a87e0d38defc4bd6a3ecb6c6d8
Vlad <lmvibe@outlook.com>
2020-01-23 06:04:52 +0300

    Release new version

--- RPForest.h
+++ RPForest.h
@@ -22,16 +22,61 @@
 
 class RPForest{
 public:
+    /** @name Constructors
+    * The constructor does not actually build the forest. The building is done
+    * by the function grow() which has to be called before queries can be made.
+    * There are two different versions of the constructor which differ only
+    * by the type of the input data. The first version takes the data set
+    * as `Ref` to `MatrixXf`, which means that the argument
+    * can be either `MatrixXf` or `Map<MatrixXf>` (also certain blocks of `MatrixXf`
+    * may be accepted, see [Eigen::Ref](https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html)
+    * for more information). The second version takes a float
+    * pointer to an array containing the data set, and the dimension and
+    * the sample size of the data. There are also corresponding versions
+    * of all the member functions which take input data. In all cases the data
+    * is assumed to be stored in column-major order such that each data point
+    * is stored contiguously in memory. In all cases no copies are made of
+    * the original data matrix. */
+
+    /**
+    * @param X_ Eigen ref to the data set, stored as one data point per column
+    */
     RPForest(const Eigen::Ref<const Eigen::MatrixXf> &X_) :
             X(Eigen::Map<const Eigen::MatrixXf>(X_.data(), X_.rows(), X_.cols())),
             n_samples(X_.cols()),
             dim(X_.rows()) {}
 
+    /**
+    * @param X_ a float array containing the data set with each data point
+    * stored contiguously in memory
+    * @param dim_ dimension of the data
+    * @param n_samples_ number of data points
+    */
     RPForest(const float *X_, int dim_, int n_samples_) :
             X(Eigen::Map<const Eigen::MatrixXf>(X_, dim_, n_samples_)),
             n_samples(n_samples_),
             dim(dim_) {}
 
+
+    /**@}*/
+
+    /** @name Forest building.
+    * Build Forest.
+    */
+
+    /**
+    * Build Forest.
+    *
+    * @param n_trees_ number of trees to be grown
+    * @param depth_ depth of the trees; in the set
+    * \f$\{1,2, \dots ,\lfloor \log_2 (n) \rfloor \}\f$, where \f$n \f$ is the number
+    * of data points
+    * @param density_ expected proportion of non-zero components in the
+    * random vectors; on the interval \f$(0,1]\f$; default value sets density to
+    * \f$ 1 / \sqrt{d} \f$, where \f$d\f$ is the dimension of the data
+    * @param seed seed given to a rng when generating random vectors;
+    * a default value 0 initializes the rng randomly with std::random_device
+    */
     void grow(int n_trees_, int depth_, float density_ = -1.0, int seed = 0){
         if(!is_empty()){
             throw std::logic_error("Forest has already been grown.");
@@ -61,7 +106,7 @@ public:
         }
 
         density < 1 ? build_sparse_random_matrix(sparse_random_matrix, n_pool, dim, density, seed) :
-                      build_dense_random_matrix(dense_random_matrix, n_pool, dim, seed);
+        build_dense_random_matrix(dense_random_matrix, n_pool, dim, seed);
 
         split_points = Eigen::MatrixXf(n_array, n_trees);
         tree_leaves = std::vector<std::vector<int>>(n_trees);
@@ -69,7 +114,7 @@ public:
         count_first_leaf_indices_all(leaf_first_indices_all, n_samples, depth);
         leaf_first_indices = leaf_first_indices_all[depth];
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int n_tree = 0; n_tree < n_trees; ++n_tree) {
             Eigen::MatrixXf tree_projections;
 
@@ -86,6 +131,11 @@ public:
         }
     }
 
+
+    /**
+    * Builds a single random projection tree. The tree is constructed by recursively
+    * projecting the data on a random vector and splitting into two by the median.
+    */
     void grow_subtree(std::vector<int>::iterator begin, std::vector<int>::iterator end,
                       int tree_level, int i, int n_tree, const Eigen::MatrixXf &tree_projections) {
         int n = end - begin;
@@ -115,6 +165,28 @@ public:
         grow_subtree(mid, end, tree_level + 1, idx_right, n_tree, tree_projections);
     }
 
+    /**@}*/
+
+    /** @name Approximate k-nn search
+    * Finds k approximate nearest neighbors
+    * from a data set X for a query point q. The indices of k nearest neighbors
+    * are written to a buffer out, which has to be preallocated to have at least
+    * length k. Optionally also Euclidean distances to these k nearest points
+    * are written to a buffer out_distances. If there are less than k points in
+    * the candidate set, -1 is written to the remaining locations of the
+    * output buffers.
+    */
+
+    /**
+    * Approximate k-nn search.
+    *
+    * @param data pointer to an array containing the query point
+    * @param k number of nearest neighbors searched for
+    * @param vote_threshold - number of votes required for a query point to be included in the candidate set
+    * @param out output buffer (size = k) for the indices of k approximate nearest neighbors
+    * @param out_distances optional output buffer (size = k) for distances to k approximate nearest neighbors
+    * @param out_n_elected optional output parameter (size = 1) for the candidate set size
+    */
     void query(const float *data, int k, int vote_threshold, int *out,
                float *out_distances = nullptr, int *out_n_elected = nullptr) const {
 
@@ -144,7 +216,7 @@ public:
         * The following loops over all trees, and routes the query to exactly one
         * leaf in each.
         */
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int n_tree = 0; n_tree < n_trees; ++n_tree) {
             int idx_tree = 0;
             for (int d = 0; d < depth; ++d) {
@@ -184,11 +256,41 @@ public:
         exact_knn(q, k, elected, n_elected, out, out_distances);
     }
 
+    /**
+   *  Approximate k-nn search.
+   *
+   * @param q Eigen ref to the query point
+   * @param k number of nearest neighbors searched for
+   * @param vote_threshold number of votes required for a query point to be included in the candidate set
+   * @param out output buffer (size = k) for the indices of k approximate nearest neighbors
+   * @param out_distances optional output buffer (size = k) for distances to k approximate nearest neighbors
+   * @param out_n_elected optional output parameter (size = 1) for the candidate set size
+   */
     void query(const Eigen::Ref<const Eigen::VectorXf> &q, int k, int vote_threshold, int *out,
                float *out_distances = nullptr, int *out_n_elected = nullptr) const {
         query(q.data(), k, vote_threshold, out, out_distances, out_n_elected);
     }
+    /**@}*/
 
+
+    /** @name Exact k-nn search
+    * Functions for fast exact k-nn search: find k nearest neighbors for a
+    * query point q from a data set X_. The indices of k nearest neighbors are
+    * written to a buffer out, which has to be preallocated to have at least
+    * length k. Optionally also the Euclidean distances to these k nearest points
+    * are written to a buffer out_distances. There are both static and member
+    * versions.
+    */
+
+    /**
+    * @param q_data pointer to an array containing the query point
+    * @param X_data pointer to an array containing the data set
+    * @param dim dimension of data
+    * @param n_samples number of points in a data set
+    * @param k number of neighbors searched for
+    * @param out output buffer (size = k) for the indices of k nearest neighbors
+    * @param out_distances optional output buffer (size = k) for the distances to k nearest neighbors
+    */
     static void exact_knn(const float *q_data, const float *X_data, int dim, int n_samples,
                           int k, int *out, float *out_distances = nullptr) {
 
@@ -201,7 +303,7 @@ public:
 
         Eigen::VectorXf distances(n_samples);
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int i = 0; i < n_samples; ++i)
             distances(i) = (X.col(i) - q).squaredNorm();
 
@@ -230,6 +332,12 @@ public:
         }
     }
 
+    /**
+    * @param q pointer to an array containing the query point
+    * @param k number of points searched for
+    * @param out output buffer (size = k) for the indices of k nearest neighbors
+    * @param out_distances optional output buffer (size = k) for the distances to k nearest neighbors
+    */
     void exact_knn(const Eigen::Map<const Eigen::VectorXf> &q, int k, const Eigen::VectorXi &indices,
                    int n_elected, int *out, float *out_distances = nullptr) const {
 
@@ -247,7 +355,7 @@ public:
 
         Eigen::VectorXf distances(n_elected);
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int i = 0; i < n_elected; ++i)
             distances(i) = (X.col(indices(i)) - q).squaredNorm();
 
@@ -277,21 +385,45 @@ public:
         }
     }
 
+    /**
+    * @param q Eigen ref to a query point
+    * @param X Eigen ref to a data set
+    * @param k number of neighbors searched for
+    * @param out output buffer (size = k) for the indices of k nearest neighbors
+    * @param out_distances optional output buffer (size = k) for the distances to k nearest neighbors
+    */
     static void exact_knn(const Eigen::Ref<const Eigen::VectorXf> &q,
                           const Eigen::Ref<const Eigen::MatrixXf> &X,
                           int k, int *out, float *out_distances = nullptr) {
         RPForest::exact_knn(q.data(), X.data(), X.rows(), X.cols(), k, out, out_distances);
     }
 
+    /**
+    * @param q pointer to an array containing the query point
+    * @param k number of neighbors searched for
+    * @param out output buffer (size = k) for the indices of k nearest neighbors
+    * @param out_distances optional output buffer (size = k) for the distances to k nearest neighbors
+    */
     void exact_knn(const float *q, int k, int *out, float *out_distances = nullptr) const {
         RPForest::exact_knn(q, X.data(), dim, n_samples, k, out, out_distances);
     }
 
+    /**
+   * @param q pointer to an array containing the query point
+   * @param k number of points searched for
+   * @param out output buffer (size = k) for the indices of k nearest neighbors
+   * @param out_distances optional output buffer (size = k) for the distances to k nearest neighbors
+   */
     void exact_knn(const Eigen::Ref<const Eigen::VectorXf> &q, int k, int *out,
                    float *out_distances = nullptr) const {
         RPForest::exact_knn(q.data(), X.data(), dim, n_samples, k, out, out_distances);
     }
 
+    /**
+    * Computes the leaf sizes of a tree assuming a median split and that
+    * when the number points is odd, the extra point is always assigned to
+    * to the left branch.
+    */
     static void count_leaf_sizes(int n, int level, int tree_depth, std::vector<int> &out_leaf_sizes) {
         if (level == tree_depth) {
             out_leaf_sizes.push_back(n);
@@ -302,6 +434,12 @@ public:
         count_leaf_sizes(n / 2, level + 1, tree_depth, out_leaf_sizes);
     }
 
+    /**
+   * Computes indices of the first elements of leaves in a vector containing
+   * all the leaves of a tree concatenated. Assumes that median split is used
+   * and when the number points is odd, the extra point is always assigned to
+   * the left branch.
+   */
     static void count_first_leaf_indices(std::vector<int> &indices, int n, int depth) {
         std::vector<int> leaf_sizes;
         count_leaf_sizes(n, 0, depth, leaf_sizes);
@@ -320,6 +458,16 @@ public:
         }
     }
 
+
+    /**
+    * Builds a random sparse matrix for use in random projection. The components of
+    * the matrix are drawn from the distribution
+    *
+    *       0 w.p. 1 - a
+    * N(0, 1) w.p. a
+    *
+    * where a = density.
+    */
     static void build_sparse_random_matrix(Eigen::SparseMatrix<float, Eigen::RowMajor> &sparse_random_matrix,
                                            int n_row, int n_col, float density, int seed = 0) {
         sparse_random_matrix = Eigen::SparseMatrix<float, Eigen::RowMajor>(n_row, n_col);
@@ -342,6 +490,11 @@ public:
         sparse_random_matrix.makeCompressed();
     }
 
+
+    /*
+    * Builds a random dense matrix for use in random projection. The components of
+    * the matrix are drawn from the standard normal distribution.
+    */
     static void build_dense_random_matrix(Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> &dense_random_matrix,
                                           int n_row, int n_col, int seed = 0) {
         dense_random_matrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>(n_row, n_col);
@@ -353,6 +506,121 @@ public:
 
         std::generate(dense_random_matrix.data(), dense_random_matrix.data() + n_row * n_col,
                       [&normal_dist, &gen] { return normal_dist(gen); });
+    }
+
+
+    /**@}*/
+
+    /** @name Utility functions
+    * Saving and loading forest and checking if it is already constructed.
+    * The same data set used to build a saved forest has to be used to
+    * construct the forest into which it is loaded.
+    */
+
+    /**
+    * Saves the forest to a file.
+    *
+    * @param path - filepath to the output file.
+    * @return true if saving succeeded, false otherwise.
+    */
+    bool save(const char *path) const {
+        FILE *fd;
+        if ((fd = fopen(path, "wb")) == NULL)
+            return false;
+
+        fwrite(&n_trees, sizeof(int), 1, fd);
+        fwrite(&depth, sizeof(int), 1, fd);
+        fwrite(&density, sizeof(float), 1, fd);
+
+        fwrite(split_points.data(), sizeof(float), n_array * n_trees, fd);
+
+        // save tree leaves
+        for (int i = 0; i < n_trees; ++i) {
+            int sz = tree_leaves[i].size();
+            fwrite(&sz, sizeof(int), 1, fd);
+            fwrite(&tree_leaves[i][0], sizeof(int), sz, fd);
+        }
+
+        // save random matrix
+        if (density < 1) {
+            int non_zeros = sparse_random_matrix.nonZeros();
+            fwrite(&non_zeros, sizeof(int), 1, fd);
+            for (int k = 0; k < sparse_random_matrix.outerSize(); ++k) {
+                for (Eigen::SparseMatrix<float, Eigen::RowMajor>::InnerIterator it(sparse_random_matrix, k); it; ++it) {
+                    float val = it.value();
+                    int row = it.row(), col = it.col();
+                    fwrite(&row, sizeof(int), 1, fd);
+                    fwrite(&col, sizeof(int), 1, fd);
+                    fwrite(&val, sizeof(float), 1, fd);
+                }
+            }
+        } else {
+            fwrite(dense_random_matrix.data(), sizeof(float), n_pool * dim, fd);
+        }
+
+        fclose(fd);
+        return true;
+    }
+
+    /**
+    * Loads an index from a file.
+    *
+    * @param path filepath to the index file.
+    * @return true if loading succeeded, false otherwise.
+    */
+    bool load(const char *path) {
+        FILE *fd;
+        if ((fd = fopen(path, "rb")) == NULL)
+            return false;
+
+        fread(&n_trees, sizeof(int), 1, fd);
+        fread(&depth, sizeof(int), 1, fd);
+        fread(&density, sizeof(float), 1, fd);
+
+        n_pool = n_trees * depth;
+        n_array = 1 << (depth + 1);
+
+        count_first_leaf_indices_all(leaf_first_indices_all, n_samples, depth);
+        leaf_first_indices = leaf_first_indices_all[depth];
+
+        split_points = Eigen::MatrixXf(n_array, n_trees);
+        fread(split_points.data(), sizeof(float), n_array * n_trees, fd);
+
+        // load tree leaves
+        tree_leaves = std::vector<std::vector<int>>(n_trees);
+        for (int i = 0; i < n_trees; ++i) {
+            int sz;
+            fread(&sz, sizeof(int), 1, fd);
+            std::vector<int> leaves(sz);
+            fread(&leaves[0], sizeof(int), sz, fd);
+            tree_leaves[i] = leaves;
+        }
+
+        // load random matrix
+        if (density < 1) {
+            int non_zeros;
+            fread(&non_zeros, sizeof(int), 1, fd);
+
+            sparse_random_matrix = Eigen::SparseMatrix<float>(n_pool, dim);
+            std::vector<Eigen::Triplet<float>> triplets;
+            for (int k = 0; k < non_zeros; ++k) {
+                int row, col;
+                float val;
+                fread(&row, sizeof(int), 1, fd);
+                fread(&col, sizeof(int), 1, fd);
+                fread(&val, sizeof(float), 1, fd);
+                triplets.push_back(Eigen::Triplet<float>(row, col, val));
+            }
+
+            sparse_random_matrix.setFromTriplets(triplets.begin(), triplets.end());
+            sparse_random_matrix.makeCompressed();
+        } else {
+            dense_random_matrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>(n_pool, dim);
+            fread(dense_random_matrix.data(), sizeof(float), n_pool * dim, fd);
+        }
+
+        fclose(fd);
+        return true;
     }
 
     bool is_empty() const {
--- main.cpp
+++ main.cpp
@@ -31,143 +31,142 @@
 using namespace Eigen;
 
 struct RPFparams {
-	int n;
-	int ntest;
-	int k;
-	int n_trees;
-	int depth;
-	int dim;
-	int votes;
-	float density;
-	bool parallel;
-	bool verbose;
-	std::string dsfolder;
-	int n_points;
+    int n;
+    int ntest;
+    int k;
+    int n_trees;
+    int depth;
+    int dim;
+    int votes;
+    float density;
+    bool parallel;
+    bool verbose;
+    std::string dsfolder;
+    int n_points;
 };
 
 void read_config(RPFparams &pars) {
-	std::ifstream cFile("config.txt");
-	if (cFile.is_open())
-	{
-		std::string line;
-		while (getline(cFile, line)) {
-			line.erase(std::remove_if(line.begin(), line.end(),
-				[](unsigned char x) { return isspace(x); }),
-				line.end());
-			if (line[0] == '#' || line.empty())
-				continue;
-			else break;
-		}
-		pars.dsfolder = line.substr(line.find("=") + 1);
-		getline(cFile, line);
-		pars.n = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.ntest = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.dim = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.k = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.n_trees = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.depth = std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.density = (float)std::atof(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.parallel = (bool)std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.verbose = (bool)std::atoi(line.substr(line.find("=") + 1).c_str());
-		getline(cFile, line);
-		pars.votes = std::atoi(line.substr(line.find("=") + 1).c_str());
-		if (!pars.dsfolder.empty() && pars.dsfolder.back() != '/' && pars.dsfolder.back() != '\\')
-			pars.dsfolder += '/';
-		pars.n_points = pars.n - pars.ntest;
-	}
-	else {
-		std::cerr << "Couldn't open config file for reading.\n";
-	}
+    std::ifstream cFile("config.txt");
+    if (cFile.is_open())
+    {
+        std::string line;
+        while (getline(cFile, line)) {
+            line.erase(std::remove_if(line.begin(), line.end(),
+                                      [](unsigned char x) { return isspace(x); }),
+                       line.end());
+            if (line[0] == '#' || line.empty())
+                continue;
+            else break;
+        }
+        pars.dsfolder = line.substr(line.find("=") + 1);
+        getline(cFile, line);
+        pars.n = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.ntest = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.dim = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.k = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.n_trees = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.depth = std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.density = (float)std::atof(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.parallel = (bool)std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.verbose = (bool)std::atoi(line.substr(line.find("=") + 1).c_str());
+        getline(cFile, line);
+        pars.votes = std::atoi(line.substr(line.find("=") + 1).c_str());
+        if (!pars.dsfolder.empty() && pars.dsfolder.back() != '/' && pars.dsfolder.back() != '\\')
+            pars.dsfolder += '/';
+        pars.n_points = pars.n - pars.ntest;
+    }
+    else {
+        std::cerr << "Couldn't open config file for reading.\n";
+    }
 }
 
 
 int main() {
-	RPFparams p;
-	std::cout << "---------------------------------------------" << std::endl;
-	std::cout << "Random Projection Forest by Vladislav Tishin." << std::endl;
-	std::cout << "---------------------------------------------" << std::endl << std::endl;
-	read_config(p);
+    RPFparams p;
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "Random Projection Forest by Vladislav Tishin." << std::endl;
+    std::cout << "---------------------------------------------" << std::endl << std::endl;
+    read_config(p);
 
-	//get data
-	float *train, *test;
+    //get data
+    float *train, *test;
 
-	test = read_memory((p.dsfolder + "test.bin").c_str(), p.ntest, p.dim);
-	if (!test) {
-		std::cerr << "In file " << __FILE__ << ", line " << __LINE__ << ": test data " << p.dsfolder + "test.bin" << " could not be read\n";
-		return -1;
-	}
+    test = read_memory((p.dsfolder + "test.bin").c_str(), p.ntest, p.dim);
+    if (!test) {
+        std::cerr << "In file " << __FILE__ << ", line " << __LINE__ << ": test data " << p.dsfolder + "test.bin" << " could not be read\n";
+        return -1;
+    }
 
-	train = read_memory((p.dsfolder + "train.bin").c_str(), p.n_points, p.dim);
-	if (!train) {
-		std::cerr << "In file " << __FILE__ << ", line " << __LINE__ << ": training data " << p.dsfolder + "train.bin" << " could not be read\n";
-		return -1;
-	}
+    train = read_memory((p.dsfolder + "train.bin").c_str(), p.n_points, p.dim);
+    if (!train) {
+        std::cerr << "In file " << __FILE__ << ", line " << __LINE__ << ": training data " << p.dsfolder + "train.bin" << " could not be read\n";
+        return -1;
+    }
 
-	//Building forest
-	if (!p.parallel) omp_set_num_threads(1);
+    //Building forest
+    if (!p.parallel) omp_set_num_threads(1);
 
+    std::cout << "Growing forest..." << std::endl;
+    double build_start = omp_get_wtime();
+    RPForest index_dense(train, p.dim, p.n_points);
+    index_dense.grow(p.n_trees, p.depth);
+    double build_time = omp_get_wtime() - build_start;
+    std::cout << "Growing Time: " << build_time << std::endl << std::endl;
 
-	std::cout << "Growing forest..." << std::endl;
-	double build_start = omp_get_wtime();
-	RPForest index_dense(train, p.dim, p.n_points);
-	index_dense.grow(p.n_trees, p.depth);
-	double build_time = omp_get_wtime() - build_start;
-	std::cout << "Growing Time: " << build_time << std::endl << std::endl;
+    //Testing
+    std::cout << "Testing..." << std::endl;
+    float total_acc = 0.0f;
+    double total_rpt = 0.0;
+    double total_bft = 0.0;
+    for (int i = 0; i < p.ntest; ++i) {
+        std::vector<int> result(p.k);
+        const Map<const VectorXf> q(&test[i * p.dim], p.dim);
+        const Map<const MatrixXf> X(train, p.dim, p.n_points);
 
+        VectorXi indices_exact(p.k);
+        double start = omp_get_wtime();
+        RPForest::exact_knn(q, X, p.k, indices_exact.data());
+        double end = omp_get_wtime();
+        double def_time = end - start;
+        total_bft += def_time;
 
-	//Testing
-	std::cout << "Testing...";
-	float total_acc = 0.0f;
-	double total_rpt = 0.0;
-	double total_bft = 0.0;
-	for (int i = 0; i < p.ntest; ++i) {
-		std::vector<int> result(p.k);
-		const Map<const VectorXf> q(&test[i * p.dim], p.dim);
-		const Map<const MatrixXf> X(train, p.dim, p.n_points);
+        start = omp_get_wtime();
+        index_dense.query(q, p.k, p.votes, &result[0]);
+        end = omp_get_wtime();
+        double time = end - start;
+        total_rpt += time;
 
-		VectorXi indices_exact(p.k);
-		double start = omp_get_wtime();
-		RPForest::exact_knn(q, X, p.k, indices_exact.data());
-		double end = omp_get_wtime();
-		double def_time = end - start;
-		total_bft += def_time;
+        float accuracy = 0.0;
+        for (int i = 0; i < p.k; ++i) {
+            accuracy += (result[i] == indices_exact(i));
+        }
+        accuracy /= p.k;
+        total_acc += accuracy;
 
-		start = omp_get_wtime();
-		index_dense.query(q, p.k, p.votes, &result[0]);
-		end = omp_get_wtime();
-		double time = end - start;
-		total_rpt += time;
+        if (p.verbose) {
+            std::cout << indices_exact.transpose() << std::endl;
+            for (int &y : result) {
+                std::cout << y << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "Accuracy: " << setprecision(3) << accuracy << '\t' << "RPTime: " << time << '\t'
+                      << "BFTime: " << def_time << std::endl << std::endl;
+        }
+    }
 
-		float accuracy = 0.0;
-		for (int i = 0; i < p.k; ++i) {
-			accuracy += (result[i] == indices_exact(i));
-		}
-		accuracy /= p.k;
-		total_acc += accuracy;
+    std::cout << "Results:" << std::endl;
+    std::cout << " - Accuracy: " << total_acc / p.ntest << "\n - Average RPTime: " << total_rpt / p.ntest << "\n";
 
-		if (p.verbose) {
-			std::cout << indices_exact.transpose() << std::endl;
-			for (int &y : result) {
-				std::cout << y << " ";
-			}
-			std::cout << std::endl;
-			std::cout << "Accuracy: " << setprecision(3) << accuracy << '\t' << "RPTime: " << time << '\t'
-				<< "BFTime: " << def_time << std::endl << std::endl;
-		}
-	}
-	std::cout << "Results:" << std::endl;
-	std::cout << " - Accuracy: " << total_acc / p.ntest << "\n - Average RPTime: " << total_rpt / p.ntest << "\n - F: " << total_bft / total_rpt << "\n";
+    delete[] test;
+    delete[] train;
 
-	delete[] test;
-	delete[] train;
-	system("pause");
-	return 0;
+    return 0;
 }
